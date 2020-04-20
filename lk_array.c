c37d508f311c9f39c26afb7d44d6b7b0657988de
Lion Kortlepel <development@kortlepel.com>
2020-04-20 09:25:28 +0200

    Add new/free mechanisms

--- lk_array.c
+++ lk_array.c
@@ -1,1 +1,26 @@
-#include "array.h"
+#include "lk_array.h"
+
+LkArray* lk_new_array(size_t size, size_t memb_size) {
+    LkArray* arr = lk_new(LkArray);
+    if (!arr)
+        return NULL;
+
+    arr->data      = LK_CALLOC(size, memb_size);
+    arr->memb_size = memb_size;
+    arr->size      = size;
+
+    if (!arr->data) {
+        LK_FREE(arr);
+        return NULL;
+    }
+
+    return arr;
+}
+
+void lk_free_array_internal(LkArray* ptr) {
+    if (!ptr) {
+        return;
+    }
+    free(ptr->data);
+    free(ptr);
+}
--- lk_array.h
+++ lk_array.h
@@ -1,5 +1,33 @@
 #ifndef LK_ARRAY_H
 #define LK_ARRAY_H
 
+#include "userdef_memory.h"
+/* From this point onwards only macros should be used for
+ * memory allocation / deallocation / reallocation.
+ * Namely these are
+ * - LK_MALLOC,        
+ * - LK_FREE,
+ * - LK_CALLOC,
+ * - LK_REALLOC,
+ * - LK_REALLOCARRAY.
+ * These may be overridden by defining them before this include.
+ */
+
+#define lk_new(type) (type*)LK_MALLOC(sizeof(type))
+
+typedef struct {
+    void*  data;
+    size_t memb_size;
+    size_t size;
+} LkArray;
+
+#define lk_free_array(ptr)           \
+    do {                             \
+        lk_free_array_internal(ptr); \
+        ptr = NULL;                  \
+    } while (0)
+
+LkArray* lk_new_array(size_t size, size_t member_size);
+void     lk_free_array_internal(LkArray* ptr);
 
 #endif // LK_ARRAY_H
